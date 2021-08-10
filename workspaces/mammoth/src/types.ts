7b42ffab3d5dbfe8b4232c80df53c3a459be260f
Martijn de Haan <178230+martijndeh@users.noreply.github.com>
2021-08-10 14:37:49 +0300

    Create nicer snapshots

--- workspaces/mammoth/src/types.ts
+++ workspaces/mammoth/src/types.ts
@@ -11,9 +11,18 @@ export type GetReturning<TableColumns, ColumnName extends keyof TableColumns> = 
   [K in ColumnName]: TableColumns[K];
 };
 
-export class GetDataType<Type, IsNotNull extends boolean> {
-  private _!: Type & IsNotNull;
+export class Maybe<T> {
+  private _!: T;
 }
+
+// This is used to create a snapshot of the data type without erasing null or undefined. If the type
+// is nullabe (not not null) the type is wrapped in a Maybe<T> helper class which is written in the
+// snapshot as-is.
+export type GetDataType<Type, IsNotNull extends boolean> = [IsNotNull] extends [true]
+  ? [true] extends [IsNotNull]
+    ? Type
+    : Maybe<Type>
+  : Maybe<Type>;
 
 export type QueryExecutorFn = (
   query: string,
